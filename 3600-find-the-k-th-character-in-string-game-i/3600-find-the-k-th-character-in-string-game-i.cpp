8a4a14eb5e799f49cbe1c216c629166c1ad245dd
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-07-03 22:02:33 +0530

    Time: 4 ms (36.05%), Space: 10 MB (58.94%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-   
+     char mp(char c) {
+    return (c == 'z') ? 'a' : c + 1;
+}
     char kthCharacter(int k) {
-        unordered_map<char, char> mp = {
-    {'a', 'b'}, {'b', 'c'}, {'c', 'd'}, {'d', 'e'}, {'e', 'f'},
-    {'f', 'g'}, {'g', 'h'}, {'h', 'i'}, {'i', 'j'}, {'j', 'k'},
-    {'k', 'l'}, {'l', 'm'}, {'m', 'n'}, {'n', 'o'}, {'o', 'p'},
-    {'p', 'q'}, {'q', 'r'}, {'r', 's'}, {'s', 't'}, {'t', 'u'},
-    {'u', 'v'}, {'v', 'w'}, {'w', 'x'}, {'x', 'y'}, {'y', 'z'},
-    {'z', 'a'}
-};
-
         string str = "a" ; 
         while( str.size() < k ){
             string temp ="";
             for( char&ch : str ){
-                temp += mp[ch] ; 
+                temp += mp(ch) ; 
             }
             str += temp ;
         }
