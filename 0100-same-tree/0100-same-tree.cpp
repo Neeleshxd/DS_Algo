c4fdb5483f11698a41e63a75498afab2410ae0e0
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-07-07 22:20:20 +0530

    Time: 0 ms (100%), Space: 12.9 MB (14.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,11 +12,32 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if( p == nullptr && q == nullptr ) return true ; 
+        if( p == nullptr &&  q == nullptr ) return true ; 
         if( p == nullptr || q == nullptr ) return false ; 
+        queue<TreeNode*>que1 ;
+        queue<TreeNode*>que2 ; 
+        que1.push(p) ;
+        que2.push(q) ; 
+        while(!que1.empty() && !que2.empty()){
+            TreeNode*front1 = que1.front() ;que1.pop(); 
+            TreeNode*front2 = que2.front() ;que2.pop();
 
-        if ( p->val != q->val ) return false ; 
-
-        return isSameTree(p->right,q->right)&&isSameTree(p->left,q->left) ; 
+            if( front1->val != front2->val ) {
+                return false ; 
+            } 
+            if(front1->left != nullptr && front2->left != nullptr ){
+                que1.push(front1->left) ; 
+                que2.push(front2->left) ; 
+            }else if(front1->left != nullptr || front2->left != nullptr){
+                return false ; 
+            }
+            if(front1->right != nullptr && front2->right != nullptr ){
+                que1.push(front1->right) ; 
+                que2.push(front2->right) ;
+            }else if (front1->right != nullptr || front2->right != nullptr){
+                return false ; 
+            }
+        }
+        return true ; 
     }
 };
