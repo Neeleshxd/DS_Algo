83f98cd1e4462c8eb2a20733437fe7772ff62365
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-07-20 18:06:09 +0530

    Time: 0 ms (100%), Space: 9.9 MB (79.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-bool alphanum(char c ){
-    return(c>='A'&& c<='Z' || c >='a' && c<='z' || c >='0' && c<='9');
+    bool isalpha(char& c ){
+      return(c>='A'&& c<='Z' || c >='a' && c<='z' || c >='0' && c<='9');
 }
     bool isPalindrome(string s) {
-        int n = s.length();
-        int i = 0 ;
-        int j = n-1;
+        int i=0 ;
+        int j = s.size() - 1; 
+
         while( i < j ){
-            while( i < j && !alphanum(s[i])){
-                i++;
-            }
-            while( i < j && !alphanum(s[j])){
-                j--;
-            }
-            if(tolower(s[i]) != tolower(s[j])){
-                return false ; 
-            }
+            while( i < j && !isalpha(s[i])) i++;
+             
+            while(i < j && !isalpha(s[j])) j--; 
+            
+            if( tolower(s[i]) != tolower(s[j])) return false; 
             i++;
-            j--;
+            j--; 
         }
         return true ; 
     }
