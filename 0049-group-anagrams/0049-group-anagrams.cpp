a684aac053d30bbf7e4d0d2f0e8fe4436c7bf993
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-07-25 23:11:21 +0530

    Time: 19 ms (48.96%), Space: 26 MB (55.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
+    string generate_word(string & word ){
+        int arr[26] = {0} ;  
+        for( char&ch : word ){
+            arr[ch-'a']++; 
+        }
+        string new_word = "" ; 
+        for( int i = 0 ;i < 26 ; i++ ){
+            int freq = arr[i] ;
+
+            if( freq > 0 ){
+                new_word += string(freq,i+'a') ; 
+            }
+        }
+        return new_word ; 
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size() ; 
         unordered_map<string,vector<string>>mp ; 
         vector<vector<string>>result  ; 
        for( string word : strs ){
-           string sorted_word = word ;
-           sort(sorted_word.begin() , sorted_word.end()) ; 
-           mp[sorted_word].push_back(word) ; 
+           string new_word = generate_word(word) ;
+           mp[new_word].push_back(word) ; 
        }
        for(auto&it:mp ){
           result.push_back(it.second) ; 
