344e39c9c7706720a7b1d39c0c4b804e0e4e1630
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-07-30 23:02:05 +0530

    Time: 0 ms (100%), Space: 18.1 MB (20.3%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0;
-        int h = n - 1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (nums[mid] == target) {
-                return true;
-            }if(nums[l] == nums[mid] && nums[mid] == nums[h] ){
-                l=l+1;
-                h=h-1;
-                continue;
-            }
-           if( nums[l] <= nums[mid]){
-            if( nums[l] <= target && target <= nums[mid]){
-                h = mid-1;
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size() ; 
+        int l = 0 ;
+        int h = n-1 ; 
+        while( l <= h ){
+          int mid = l+(h-l) / 2 ; 
+          if(nums[mid] == target ) return true  ; 
+          if( nums[l] == nums[mid]&& nums[mid] == nums[h]){
+            l++; 
+            h--;
+            continue ; 
+          }
+          if( nums[l] <= nums[mid]){
+            if( nums[l]<= target &&  nums[mid] >= target ){
+                h = mid -1 ; 
             }else{
-                l = mid+1;
+                l = mid +1 ; 
             }
-           }else{
-            if( nums[mid] <= target && target <= nums[h]){
-             l = mid+1;
+          }else if(nums[l] > nums[mid]){
+            if(  nums[mid] <= target && nums[h] >= target ){
+                l = mid +1 ; 
             }else{
-                h = mid-1 ; 
+                h = mid -1 ; 
             }
-           }
+          }
         }
-        return false;
+        return false  ; 
     }
 };
