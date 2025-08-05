acb048bacd045800191a67e70e094e658658bea4
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-08-06 02:41:38 +0530

    Time: 0 ms (100%), Space: 14.4 MB (42.66%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    vector<int>temp ;  
+    vector<vector<int>>result ; 
+    void solve(vector<int>& candidates,int target ,int idx){
+        int n = candidates.size() ; 
+        if( target == 0 ){
+          result.push_back(temp) ;  
+          return ; 
+        }
+        if( target < 0 ) return ; 
+        for( int i = idx ; i < n ; i++ ){
+            temp.push_back(candidates[i]) ; //do
+            solve(candidates,target-candidates[i],i) ; // explore
+            temp.pop_back() ; // undo  
+        }
+    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
+        solve(candidates,target,0) ; 
+        return result ; 
     }
 };
