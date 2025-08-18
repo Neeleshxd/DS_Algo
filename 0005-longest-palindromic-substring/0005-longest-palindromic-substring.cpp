b81ce97b8f1737f203bfd252d21d2da8ca32d0fb
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-08-19 00:02:42 +0530

    Time: 175 ms (26.13%), Space: 13.6 MB (55.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,31 @@
 class Solution {
-public:
-    bool isPalindrome(const string& s, int left, int right) {
-        while (left < right) {
-            if (s[left] != s[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
+  public:
+  int t[1001][1001]  ; 
+    bool isPalindrome(int i ,int j , string&s ){
+        if( i >= j ) {
+            return 1 ; }
+        if( t[i][j] != -1 ) {
+            return t[i][j] ;} 
+        if( s[i] == s[j] )
+            return t[i][j] = isPalindrome(i+1,j-1,s) ; 
+        
+            return t[i][j] = 0 ; 
     }
-
-    string longestPalindrome(string s) {
-        int n = s.length();
-        int maxLen = 0;
-        int start = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j) && (j - i + 1 > maxLen)) {
-                    start = i;
-                    maxLen = j - i + 1;
-                }
-            }
-        }
-
-        return s.substr(start, maxLen);
+    string longestPalindrome(string &s) {
+        int n = s.length() ;
+         memset(t,-1,sizeof(t)) ; 
+        int max_length =  1 ; 
+        int start = 0; 
+       for( int i = 0 ; i < n ; i++ ){
+           for(int j = i ; j < n ; j++ ){
+               if(isPalindrome(i,j,s)){
+                   if( max_length < j-i+1 ){
+                      max_length = max(max_length,j-i+1) ; 
+                      start = i ; 
+                   } 
+               }
+           }
+       }
+        return s.substr(start,max_length) ; 
     }
 };
