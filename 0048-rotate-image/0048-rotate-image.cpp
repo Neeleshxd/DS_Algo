bef7f67e224b92f4e899e4cdd32b8466587e8ec4
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-08-27 23:39:04 +0530

    Time: 0 ms (100%), Space: 10.1 MB (92.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,12 +4,12 @@ public:
         int n = matrix.size() ;
         int m = matrix[0].size() ;
 
-        for( int i = 0; i < n ; i++ ){
+        for( int i = 0; i < n ; i++ ){ // Transpose find karra 
             for( int j = i+1 ; j < m ;j++ ){
                 swap(matrix[i][j],matrix[j][i]) ; 
             }
         }
-        for( int j = 0 ;j < n ;j++ ){
+        for( int j = 0 ;j < n ;j++ ){ // We just reverse the rows in this case from i to k  where i is travesing the row till k 
             for( int i = 0 , k = m-1 ; i < k ;i++,k--){
                 swap(matrix[j][i],matrix[j][k]) ; 
             }
