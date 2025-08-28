ec8273e9e510efca9db0443d2ff10a5965a70aca
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-08-28 09:58:29 +0530

    Time: 10 ms (53.93%), Space: 11.9 MB (58.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -10,8 +10,8 @@ public:
         while( j < n ){
             mp[s[j]]++; //a , b ,c 
             while( mp[s[j]] > 1 ){
-                mp[s[i]]--;
-                i++; 
+                mp[s[i]]--;// freq out 
+                i++; //shrink the window 
             }
          max_sub = max(max_sub,j-i+1) ; 
          j++; 
