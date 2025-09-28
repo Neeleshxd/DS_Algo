dcaef94d51b64e588ba9d66e116ce0bb50ca0059
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-09-28 18:57:58 +0530

    Time: 3 ms (76.43%), Space: 25.5 MB (76.13%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -6,11 +6,12 @@ public:
         int max_perimeter = 0 ; 
        sort(nums.begin(),nums.end()); 
        for( int k = n-1 ; k >= 2 ; k-- ){
-           int i = 0 ;
            int j = k-1 ; 
+           int i = j-1 ;
            while( i < j ){
             if( nums[i] + nums[j] > nums[k] ){
-                max_perimeter = max(max_perimeter,nums[i]+nums[j]+nums[k]) ; 
+               max_perimeter = nums[i]+nums[j]+nums[k] ;  
+               return max_perimeter ; 
             }
                 i++;            
            }
