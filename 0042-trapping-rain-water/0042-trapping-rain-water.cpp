bcba6d1acc6dea5242dd4f84537f1d10fd6757f9
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-10-15 00:19:22 +0530

    Time: 3 ms (18.38%), Space: 27.2 MB (31.03%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,38 +1,31 @@
 class Solution {
-  public:
-    void leftmax(vector<int>&left_max,vector<int> &arr){
-        int n = left_max.size() ; 
-        
-        left_max[0] = arr[0] ; 
-        
-        for( int i = 1 ; i < n ;i++ ){
-            left_max[i] = max(left_max[i-1],arr[i]) ; 
+public:
+    void  left_max( vector<int>&leftmax,vector<int>&height){
+        int n = height.size() ; 
+        leftmax[0] =  height[0] ; 
+        for( int i =1 ; i < n ; i++ ){
+           leftmax[i] = max(leftmax[i-1],height[i]) ; 
         }
     }
-     void rightmax(vector<int>&right_max,vector<int> &arr){
-        int n = right_max.size() ; 
-        
-        right_max[n-1] = arr[n-1] ; 
-        
-        for( int i = n-2 ; i >=0  ;i-- ){
-            right_max[i] = max(right_max[i+1],arr[i]) ; 
+     void  right_max( vector<int>&rightmax,vector<int>&height){
+        int n = height.size() ; 
+        rightmax[n-1] =height[n-1] ;  
+        for( int i =n-2 ; i >=0  ; i-- ){
+           rightmax[i] = max(rightmax[i+1],height[i]) ; 
         }
     }
-    int trap(vector<int> &arr) {
-        // code here
-        int n =arr.size() ;
-        
-        vector<int>left_max(n,0) ;
-        vector<int>right_max(n,0) ;
-        
-        leftmax(left_max,arr) ; 
-        rightmax(right_max,arr) ; 
-        
-        int sum = 0 ; 
-        for( int i = 0 ;i < n ; i++ ){
-            
-            int h = min(left_max[i],right_max[i]) - arr[i] ; 
-            
+    int trap(vector<int>& height) {
+        int n = height.size() ; 
+
+        vector<int>leftmax(n,0) ; 
+        vector<int>rightmax(n,0) ; 
+        left_max(leftmax,height) ; 
+        right_max(rightmax,height); 
+
+        int sum = 0 ;
+
+        for( int i = 0 ; i < n ;i++){
+            int h = min(leftmax[i],rightmax[i])- height[i] ; 
             sum += h ; 
         }
         return sum ; 
