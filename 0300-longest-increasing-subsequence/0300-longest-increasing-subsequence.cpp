2f2a31077f992ce83bf47d5bfe8ca4b7753db796
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-11-07 17:18:24 +0530

    Time: 431 ms (26.12%), Space: 41.7 MB (35.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-int t[2501][2501] ; 
-int solve(vector<int>& nums,int idx ,int p ){
-    int n = nums.size() ; 
-    if( idx >= n ) return 0 ;
-    if( p != -1 && t[idx][p] != -1 ) return t[idx][p] ; 
-    int take = 0 ;  
-    if( p == -1 || nums[p] < nums[idx]){
-         take = 1 + solve(nums,idx+1,idx) ; 
+    int t[2501][2501] ; 
+    int solve(vector<int>& nums,int i ,int p ){
+        int n = nums.size() ; 
+        if(p != -1 && t[i][p] != -1 ) return t[i][p] ; 
+        if( i >= n ) return 0 ; 
+         int take = 0  ; 
+        if( p == -1 || nums[p] < nums[i]){
+            take = 1 + solve(nums,i+1,i) ; 
+        }
+        int nottake = solve(nums,i+1,p) ; 
+        
+        return ((p != -1 )?t[i][p] = max(take,nottake):max(take,nottake )) ; 
     }
-    int skip = solve(nums,idx+1,p) ; 
-    return ((p != -1 )?t[idx][p] = max(take,skip):max(take,skip)) ; 
-}
     int lengthOfLIS(vector<int>& nums) {
         memset(t,-1,sizeof(t)) ; 
         return solve(nums,0,-1) ; 
