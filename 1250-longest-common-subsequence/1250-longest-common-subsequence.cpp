0cb6953dc2203e1d105dc438675ad73eb94c53f1
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-11-12 00:52:12 +0530

    Time: 30 ms (37.39%), Space: 12.9 MB (83.9%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,20 +1,19 @@
 class Solution {
   public:
-    int t[1001][1001] ; 
-    int solve(string &s1, string &s2,int i,int j ){
+  int t[1001][1001] ; 
+    int solve(string &s1, string &s2,int i ,int j ){
         int n = s1.size() ;
         int m = s2.size() ; 
-        if( i >= n  || j >= m ) return 0 ; 
+        
+        if( i >= n || j >= m ) return 0 ; 
         if( t[i][j] != -1 ) return t[i][j] ; 
         if( s1[i] == s2[j]){
-            return t[i][j] = 1 + solve(s1,s2,i+1,j+1) ; 
+            return 1+solve(s1,s2,i+1,j+1) ; 
         }
-            return t[i][j]= max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1)) ;  
-        
+        return t[i][j] = max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1)) ; 
     }
     int longestCommonSubsequence(string &s1, string &s2) {
-        // code here
-        memset(t,-1,sizeof(t)) ; 
+        memset(t,-1,sizeof(t)); 
         return solve(s1,s2,0,0) ; 
     }
 };
