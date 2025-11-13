5d72b670d2a55974a6dbddf8ded58e4de2ca60c7
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-11-13 23:40:01 +0530

    Time: 175 ms (23.87%), Space: 13.5 MB (53.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,31 +1,31 @@
 class Solution {
   public:
-  int t[1001][1001]  ; 
-    bool isPalindrome(int i ,int j , string&s ){
-        if( i >= j ) {
-            return 1 ; }
-        if( t[i][j] != -1 ) {
-            return t[i][j] ;} 
-        if( s[i] == s[j] )
-            return t[i][j] = isPalindrome(i+1,j-1,s) ; 
-        
-            return t[i][j] = 0 ; 
+  int t[1001][1001] ; 
+    bool solve(string &s,int i , int j ){
+        if( i >= j ) return 1 ; 
+        if(t[i][j] != -1 ) return t[i][j] ; 
+        if( s[i] == s[j]){
+            return t[i][j] = solve(s,i+1,j-1) ; 
+        }
+        return t[i][j] = 0; 
     }
     string longestPalindrome(string &s) {
-        int n = s.length() ;
-         memset(t,-1,sizeof(t)) ; 
-        int max_length =  1 ; 
-        int start = 0; 
-       for( int i = 0 ; i < n ; i++ ){
-           for(int j = i ; j < n ; j++ ){
-               if(isPalindrome(i,j,s)){
-                   if( max_length < j-i+1 ){
-                      max_length = max(max_length,j-i+1) ; 
-                      start = i ; 
-                   } 
-               }
-           }
-       }
-        return s.substr(start,max_length) ; 
+        // code here
+        int sp = 0 ; 
+        int n = s.size() ; 
+        memset(t,-1,sizeof(t)) ; 
+        int max_len = INT_MIN ; 
+        for( int i = 0 ; i < n ;i++){
+            for( int j = i ; j< n ; j++){
+                if(solve(s,i,j) == true ){
+                    if( max_len < j-i+1){
+                        max_len = j-i+1; 
+                        sp = i ; 
+                    }
+                }
+            }
+        }
+        return s.substr(sp,max_len) ; 
+        
     }
-};
+}; 
