ec2b69365549c30c80f92e10f557acc584cc1fc7
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-11-15 00:09:52 +0530

    Time: 38 ms (23.69%), Space: 34.5 MB (18.18%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,21 +1,23 @@
 class Solution {
-public:
-    int t[1001][1001] ; 
-    bool isPalindrome(int i ,int j , string&s){
-        if( i >= j )return true  ;
-        if( t[i][j] != -1 ) return t[i][j] ; 
+  public:
+    int n ;
+    vector<vector<int>>dp ; 
+    int solve(string&s ,int i ,int j ){
+        if( i >= j ) return true ; 
+        if(dp[i][j] != -1)return dp[i][j]; 
         if( s[i] == s[j]){
-           return t[i][j] =  isPalindrome(i+1,j-1,s) ; 
+            return dp[i][j] = solve(s,i+1,j-1) ; 
         }
-        return t[i][j] = 0  ;  
+        return false ; 
     }
-    int countSubstrings(string s) {
-        int n = s.size() ; 
+    int countSubstrings(string &s) {
+        // code here
+         n = s.size() ; 
+        dp.assign(n,vector<int>(n,-1)); 
         int count = 0 ; 
-        memset(t,-1,sizeof(t)) ; 
-        for( int i = 0 ; i < n ;i++ ){
-            for( int j = i ; j < n ; j++ ){
-                if( isPalindrome(i,j,s)){
+        for ( int i = 0 ; i < n ; i++ ){
+            for( int j = i ; j < n ; j++){
+                if( solve(s,i,j)){
                     count++; 
                 }
             }
