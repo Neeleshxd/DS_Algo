ba718064b89c767588302b0846401e643ec652e8
Neelesh Adhikari <115217555+Neeleshxd@users.noreply.github.com>
2025-11-15 17:20:07 +0530

    Time: 26 ms (63.91%), Space: 27.4 MB (68.13%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,19 +1,28 @@
 class Solution {
   public:
-  int t[1001][1001] ; 
-    int solve(string &s1, string &s2,int i ,int j ){
-        int n = s1.size() ;
-        int m = s2.size() ; 
-        
-        if( i >= n || j >= m ) return 0 ; 
-        if( t[i][j] != -1 ) return t[i][j] ; 
-        if( s1[i] == s2[j]){
-            return 1+solve(s1,s2,i+1,j+1) ; 
-        }
-        return t[i][j] = max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1)) ; 
-    }
     int longestCommonSubsequence(string &s1, string &s2) {
-        memset(t,-1,sizeof(t)); 
-        return solve(s1,s2,0,0) ; 
+        // code here
+        int m = s1.size() ; 
+        int n = s2.size() ; 
+        
+        vector<vector<int>>t(m+1,vector<int>(n+1)) ; 
+        
+        for( int row = 0 ; row < m+1 ; row++ ){
+            t[row][0] = 0 ; 
+        }
+        for( int col = 0 ; col < n+1 ; col++){
+            t[0][col] = 0 ; 
+        }
+        for( int i = 1 ; i < m+1 ;i++){
+            for( int j = 1 ; j < n+1;j++){
+                if( s1[i-1] == s2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1] ; 
+                }else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]) ; 
+                }
+            }
+        }
+        return t[m][n] ; 
     }
 };
+
